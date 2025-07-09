781fefa655336adfeca21e6babd77114456f16fc
Liviu Chis <chis@adobe.com>
2025-07-09 14:02:20 -0700

    Added endpoint config script

---
+++ scripts/endpointconfig.js
@@ -0,0 +1,9 @@
+function getAEMPublish() {
+  return 'https://publish-p153659-e1620914.adobeaemcloud.com';
+}
+
+function getAEMAuthor() {
+  return 'https://author-p153659-e1620914.adobeaemcloud.com';
+}
+
+export { getAEMPublish, getAEMAuthor };
